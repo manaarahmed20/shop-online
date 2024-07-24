e1ef56ae26bf72f25f00cabdadac299903caa058
unknown <manaarahmed20@gmail.com>
2024-07-24 14:48:33 +0300

    inital

---
+++ ShopOnline.Api/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Repositories;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>options.UseMySQL(builder.Configuration.GetConnectionString("ShopOnlineConnection"))
+);
+builder.Services.AddScoped<ProductRepository, ProductRepository>();
+builder.Services.AddScoped<ShoppingCartRepository, ShoppingCartRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
